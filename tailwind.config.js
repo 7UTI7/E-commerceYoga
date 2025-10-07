efe2141f9db4c51c3cb95643ae486d93681c6ef4
Pedro <pedro_graciani@hotmail.com>
2025-10-07 19:18:38 -0300

    base do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/views/**/*.{ejs,html}", "./public/**/*.{js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
